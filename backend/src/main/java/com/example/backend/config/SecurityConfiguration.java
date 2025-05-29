b51ddfe2fc3bf5642c1ca5351828509d919903f1
Wadowski-Grzegorz <grzeg0132@gmail.com>
2025-05-29 17:20:26 +0200

    fix(auth): fixed how initial authentication is handled, also added one endpoint to user auth range

--- backend/src/main/java/com/example/backend/config/SecurityConfiguration.java
+++ backend/src/main/java/com/example/backend/config/SecurityConfiguration.java
@@ -6,12 +6,17 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 
+import java.util.List;
 import java.util.Set;
 
 @Configuration
@@ -33,11 +38,12 @@ public class SecurityConfiguration {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
-            .csrf(csrf -> csrf.disable())
+            .csrf(AbstractHttpConfigurer::disable)
+            .cors(httpSecurityCorsConfigurer -> httpSecurityCorsConfigurer.configurationSource(corsConfigurationSource()))
             .authorizeHttpRequests(auth -> auth
                     .requestMatchers(ALLOWED_PATHS.toArray(new String[0])).permitAll()
                     .requestMatchers(HttpMethod.PUT, "/api/user/*").authenticated()
-                    .requestMatchers("/api/user/summary").authenticated()
+                    .requestMatchers("/api/user/summary", "/api/user/stats").authenticated()
                     .requestMatchers(HttpMethod.GET, "/api/drawing").hasRole("ADMIN")
                     .requestMatchers("/api/user", "/api/user/*").hasRole("ADMIN")
                     .anyRequest().authenticated()
@@ -47,6 +53,19 @@ public class SecurityConfiguration {
             .addFilterBefore(jwtAuthFilter, UsernamePasswordAuthenticationFilter.class);
 
         return http.build();
+    }
+
+    @Bean
+    CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration config = new CorsConfiguration();
+        config.setAllowedOrigins(List.of("http://localhost:5173"));
+        config.setAllowedMethods(List.of("GET","POST","PUT","DELETE","OPTIONS"));
+        config.setAllowCredentials(true);
+        config.setAllowedHeaders(List.of("*"));
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        source.registerCorsConfiguration("/**", config);
+        return source;
     }
 
 }
